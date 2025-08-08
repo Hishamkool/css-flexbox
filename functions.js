0ae49be68e28308c186123d0eec1039e885f38bb
Hisham <linkedin.hisham@gmail.com>
2025-08-08 14:07:41 +0530

    grid vs stack

--- functions.js
+++ functions.js
@@ -3,7 +3,11 @@ function updateScreenSizes() {
     boxes.forEach(
         box => {
             const rect = box.getBoundingClientRect();
-            box.textContent = `width: ${Math.round(rect.width)} px`
+            const fontSize = Math.max(10, Math.min(24, rect.width / 10)); // Scale with width
+            box.style.fontSize = `${fontSize}px`;
+
+            box.style.whiteSpace = "pre-line";
+            box.textContent = `height: ${Math.round(rect.height)}px\nwidth: ${Math.round(rect.width)} px`
         }
     );
 }
